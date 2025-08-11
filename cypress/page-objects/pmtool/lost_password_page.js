30ad549f367addf5f63f81f6990cb18d6efbd61e
Lenka Ertl <drahozalovalenka@seznam.cz>
2025-08-11 16:41:38 +0200

    Fluent interface, Moment.js

--- cypress/page-objects/pmtool/lost_password_page.js
+++ cypress/page-objects/pmtool/lost_password_page.js
@@ -1,19 +1,37 @@
+import { LoginPage } from "./login_page";
+import { LostPasswordTransfer } from "./lost_password_transfer";
+
 export class LostPasswordPage {
   constructor() {
     this.emailInput = ":nth-child(3) > .input-icon > .form-control";
     this.usernameInput = ":nth-child(2) > .input-icon > .form-control";
     this.sendButton = ".btn-info";
+    this.backButton = "#back-btn";
+  }
+
+  clickBack() {
+    cy.get(this.backButton).click();
+    return new LoginPage();
   }
 
   typeEmail(email) {
     cy.get(this.emailInput).type(email);
+    return this;
   }
 
   typeUsername(username) {
     cy.get(this.usernameInput).type(username);
+    return this;
   }
 
   clickSend() {
     cy.get(this.sendButton).click();
+    return new LoginPage();
+  }
+
+  // ? Bonusová metoda směřující na rozcestník
+  clickSendTransfer() {
+    cy.get(this.sendButton).click();
+    return new LostPasswordTransfer();
   }
 }
