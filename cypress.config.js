6ec6ae3484402688e4c83a16c388a57a0e3291d2
Lenka Ertl <drahozalovalenka@seznam.cz>
2025-09-03 21:01:41 +0200

    Testování API

--- cypress.config.js
+++ cypress.config.js
@@ -5,10 +5,14 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-
     env: {
       // ? Alternativa k cypress.env.json
       testVar: "Toto je proměnná",
+      pmtool_url: "https://tredgate.com/pmtool",
+      tegb_url: "http://localhost:3001/",
+      eshop_url: "https://tredgate.com/eshop",
+      webtrain_url: "https://tredgate.com/webtrain",
+      automation_test_store_url: "https://automationteststore.com/",
     },
     watchForFileChanges: false,
     defaultCommandTimeout: 10000,
